df7bed3806cf51a508e808521478f85ae5b14a97
bleau1990 <415626121@qq.com>
2017-03-07 23:20:58 +0800

    nothing serious

--- svm/README.md
+++ svm/README.md
@@ -3,6 +3,8 @@
 
 dataformat :
 
+
+
 #[label]   [Index1]:[value1]  [index2]:[value2]  [index3]:[value3]
 # [label]:类别（通常是整数）[index n]: 有顺序的索引 [value n]
 
--- svm/_main.py
+++ svm/_main.py
@@ -11,7 +11,7 @@ m=svm_train(y,x,'-c 8.0 -g 8.0')
 
 
 u,v=svm_read_problem("data/news20test")
-p_lable,p_acc,p_val=svm_predict(u,v,m)
-print p_lable
+p_lable,p_acc,p_val=svm_predict(y,x,m)
+#ACC, MSE, SCC  4.5, 103.165, 0.00440058054252315
 print p_acc
-print p_val
+#print p_val
